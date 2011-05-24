7b45e119e7bf7ac881db9db0bfb63e5a75560515
Johannes 'fish' Ziemke <fish@freigeist.org>
2011-05-24 16:35:46 +0200

    remove branches

--- branches/stable-0.1/lib/WWW/MeGa/Item/Audio.pm
+++
@@ -1,3 +0,0 @@
-package WWW::MeGa::Item::Audio;
-use WWW::MeGa::Item;
-our @ISA = qw(WWW::MeGa::Item);
--- branches/stable-0.1/lib/WWW/MeGa/Item/Other.pm
+++
@@ -1,3 +0,0 @@
-package WWW::MeGa::Item::Other;
-use WWW::MeGa::Item;
-our @ISA = qw(WWW::MeGa::Item);
